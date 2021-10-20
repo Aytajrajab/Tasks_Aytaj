45f1ee46075210ad6c6da74b776c60d1c317f14c
Aytajrajab <88050466+Aytajrajab@users.noreply.github.com>
2021-10-20 12:01:40 +0400

    Add files via upload

---
+++ Rectangle_area/js/script.js
@@ -0,0 +1,10 @@
+function getArea(a, b) {
+    return a*b;
+}
+
+let a = Number(prompt("Düzbucaqlının enini daxil edin: "));
+let b = Number(prompt("Düzbucaqlının uzunluğunu daxil edin: "));
+
+
+let result = getArea(a,b);
+alert(result);
